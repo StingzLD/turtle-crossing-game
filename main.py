078596675c87f837f72360bc76501f1e67c033f5
Corey Hastings <stingzld@gmail.com>
2023-01-02 12:15:09 -0600

    Created player class

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
+# Initialize screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.bgcolor("black")
+screen.listen()
+
+# Initialize player
+dog = Player()
+screen.onkey(dog.move, "Up")
+
+game_running = True
+while game_running:
+    time.sleep(0.1)
+    screen.update()
+
+screen.exitonclick()
